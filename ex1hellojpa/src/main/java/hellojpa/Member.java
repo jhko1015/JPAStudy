8721be78ae46af41257454703bc44bb4e2abde1f
unknown <jhko1015@hunet-in.hunet.co.kr>
2019-12-19 21:18:21 +0900

    Hello Github

---
+++ ex1hellojpa/src/main/java/hellojpa/Member.java
@@ -0,0 +1,34 @@
+package hellojpa;
+
+import com.sun.javafx.image.impl.ByteIndexed;
+import jdk.nashorn.internal.objects.annotations.Getter;
+import jdk.nashorn.internal.objects.annotations.Setter;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+@Entity
+public class Member {
+    @Id
+    private Long id;
+
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    //Getter, Setter …
+}
